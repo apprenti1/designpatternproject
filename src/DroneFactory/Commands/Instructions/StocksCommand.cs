8380898c6cf5adb226e04512fd4197e20bcfa61d
Elias Aitboukha <elias.aitboukha@gmail.com>
2026-07-18 14:12:00 +0200

    Commande STOCKS
    Co-authored-by: francois95140 <totograncois12@gmail.com>
    Co-authored-by: flemelle <t.bruce@myskolae.fr>

---
+++ src/DroneFactory/Commands/Instructions/StocksCommand.cs
@@ -0,0 +1,12 @@
+namespace DroneFactory.Commands.Instructions;
+
+public sealed class StocksCommand : IInstruction
+{
+    private readonly InstructionHandler _handler;
+
+    public StocksCommand(InstructionHandler handler) => _handler = handler;
+
+    public string Name => "STOCKS";
+
+    public IEnumerable<string> Execute(string args) => _handler.Stocks();
+}
