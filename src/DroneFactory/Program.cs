d4a249bf3f515470eba772883638b451af93ccd8
Elias Aitboukha <elias.aitboukha@gmail.com>
2026-07-19 19:29:00 +0200

    Cablage DI des repositories + du registre d'instructions
    Co-authored-by: francois95140 <totograncois12@gmail.com>
    Co-authored-by: flemelle <t.bruce@myskolae.fr>

--- src/DroneFactory/Program.cs
+++ src/DroneFactory/Program.cs
@@ -1,4 +1,5 @@
 using DroneFactory.Commands;
+using DroneFactory.Domain.Categories;
 using DroneFactory.Storage;
 using Microsoft.Extensions.FileProviders;
 
@@ -6,8 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(StockStore.CreateDefault());
+builder.Services.AddSingleton<IStockRepository>(StockStore.CreateDefault());
+builder.Services.AddSingleton<ITemplateRepository>(TemplateStore.CreateDefault());
 builder.Services.AddSingleton<InstructionHandler>();
+builder.Services.AddSingleton<InstructionRegistry>();
 
 var app = builder.Build();
 
@@ -21,19 +24,38 @@ var repoRootFiles = new PhysicalFileProvider(RepoPaths.FindRepoRoot());
 app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = repoRootFiles });
 app.UseStaticFiles(new StaticFileOptions { FileProvider = repoRootFiles });
 
-app.MapGet("/api/stocks", (InstructionHandler handler) =>
-    Results.Ok(new LinesResponse(handler.Stocks().ToArray())));
+app.MapGet("/api/stocks", (InstructionRegistry registry) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "STOCKS", string.Empty))));
 
-app.MapPost("/api/needed-stocks", (InstructionHandler handler, ArgsRequest request) =>
-    Results.Ok(new LinesResponse(handler.NeededStocks(request.Args).ToArray())));
+app.MapPost("/api/needed-stocks", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "NEEDED_STOCKS", request.Args))));
 
-app.MapPost("/api/instructions", (InstructionHandler handler, ArgsRequest request) =>
-    Results.Ok(new LinesResponse(handler.Instructions(request.Args).ToArray())));
+app.MapPost("/api/instructions", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "INSTRUCTIONS", request.Args))));
 
-app.MapPost("/api/verify", (InstructionHandler handler, ArgsRequest request) =>
-    Results.Ok(new LinesResponse(handler.Verify(request.Args).ToArray())));
+app.MapPost("/api/verify", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "VERIFY", request.Args))));
 
-app.MapPost("/api/produce", (InstructionHandler handler, ArgsRequest request) =>
-    Results.Ok(new LinesResponse(handler.Produce(request.Args).ToArray())));
+app.MapPost("/api/produce", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "PRODUCE", request.Args))));
+
+app.MapPost("/api/templates", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "ADD_TEMPLATE", request.Args))));
+
+app.MapGet("/api/templates", (ITemplateRepository templates) =>
+    Results.Ok(templates.All.Select(t => new TemplateInfo(
+        t.Name,
+        CategoryClassifier.Names(CategoryClassifier.Classify(t)).ToArray(),
+        t.Hull,
+        t.MainModule,
+        t.Generator,
+        t.MovementModule,
+        t.ControlModule,
+        t.System)).ToArray()));
 
 app.Run();
+
+static string[] Dispatch(InstructionRegistry registry, string name, string args)
+    => registry.TryGet(name, out var instruction)
+        ? instruction.Execute(args).ToArray()
+        : new[] { $"ERROR Unknown instruction '{name}'" };
