99eda316990c3f463da488c28783005cb6bc6739
Elias Aitboukha <elias.aitboukha@gmail.com>
2026-07-23 21:20:00 +0200

    Program : DI multi-usines et routes API de la phase 3
    Co-authored-by: francois95140 <totograncois12@gmail.com>
    Co-authored-by: flemelle <t.bruce@myskolae.fr>

--- src/DroneFactory/Program.cs
+++ src/DroneFactory/Program.cs
@@ -7,10 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IStockRepository>(StockStore.CreateDefault());
-builder.Services.AddSingleton<ITemplateRepository>(TemplateStore.CreateDefault());
-builder.Services.AddSingleton<InstructionHandler>();
-builder.Services.AddSingleton<InstructionRegistry>();
+
+var dataDirectory = RepoPaths.DataDirectory;
+var factories = new FactoryStore(new Dictionary<string, IStockRepository>
+{
+    ["Usine1"] = new StockStore(dataDirectory),
+    ["Usine2"] = new StockStore(dataDirectory, "stock.usine2"),
+});
+var templates = TemplateStore.CreateDefault();
+var orders = OrderStore.CreateDefault();
+var movements = MovementStore.CreateDefault();
+var handler = new InstructionHandler(factories, templates, orders);
+var registry = new InstructionRegistry(handler, movements);
+
+builder.Services.AddSingleton<IFactoryRegistry>(factories);
+builder.Services.AddSingleton<ITemplateRepository>(templates);
+builder.Services.AddSingleton<IOrderRepository>(orders);
+builder.Services.AddSingleton<IMovementRepository>(movements);
+builder.Services.AddSingleton(handler);
+builder.Services.AddSingleton(registry);
 
 var app = builder.Build();
 
@@ -24,8 +39,8 @@ var repoRootFiles = new PhysicalFileProvider(RepoPaths.FindRepoRoot());
 app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = repoRootFiles });
 app.UseStaticFiles(new StaticFileOptions { FileProvider = repoRootFiles });
 
-app.MapGet("/api/stocks", (InstructionRegistry registry) =>
-    Results.Ok(new LinesResponse(Dispatch(registry, "STOCKS", string.Empty))));
+app.MapGet("/api/stocks", (InstructionRegistry registry, string? inFactory) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "STOCKS", ToArgs(inFactory)))));
 
 app.MapPost("/api/needed-stocks", (InstructionRegistry registry, ArgsRequest request) =>
     Results.Ok(new LinesResponse(Dispatch(registry, "NEEDED_STOCKS", request.Args))));
@@ -48,12 +63,34 @@ app.MapGet("/api/templates", (ITemplateRepository templates) =>
         CategoryClassifier.Names(CategoryClassifier.Classify(t)).ToArray(),
         t.Hull,
         t.MainModule,
-        t.Generator,
-        t.MovementModule,
+        t.Generators.ToArray(),
+        t.MovementModules.ToArray(),
         t.ControlModule,
         t.System)).ToArray()));
 
+app.MapPost("/api/receive", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "RECEIVE", request.Args))));
+
+app.MapPost("/api/transfer", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "TRANSFER", request.Args))));
+
+app.MapPost("/api/orders", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "ORDER", request.Args))));
+
+app.MapPost("/api/orders/send", (InstructionRegistry registry, ArgsRequest request) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "SEND", request.Args))));
+
+app.MapGet("/api/orders", (InstructionRegistry registry) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "LIST_ORDER", string.Empty))));
+
+app.MapGet("/api/movements", (InstructionRegistry registry, string? args) =>
+    Results.Ok(new LinesResponse(Dispatch(registry, "GET_MOVEMENTS", args ?? string.Empty))));
+
+app.MapGet("/api/factories", (IFactoryRegistry factories) => Results.Ok(factories.Names));
+
 app.Run();
+
+static string ToArgs(string? inFactory) => string.IsNullOrWhiteSpace(inFactory) ? string.Empty : $"IN {inFactory}";
 
 static string[] Dispatch(InstructionRegistry registry, string name, string args)
     => registry.TryGet(name, out var instruction)
